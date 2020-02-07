4854fee82553c4b17d37c04bc23f0634e2d3b5c0
Naomi Aro <naomi.aro@zalando.fi>
2020-02-08 01:38:44 +0200

    build dynamic year filter

--- src/components/LaunchList/index.tsx
+++ src/components/LaunchList/index.tsx
@@ -1,4 +1,10 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, {
+  FunctionComponent,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from 'react';
+import Select, { ValueType } from 'react-select';
 import styles from './LaunchList.module.css';
 import { SpaceXAPILaunch } from 'SpaceXContext';
 import { LaunchItem } from 'components/LaunchItem';
@@ -10,11 +16,43 @@ type LaunchListProps = {
 type SortDirection = 'asc' | 'desc';
 type Year = number | null;
 
+type OptionType = { label: Year; value: Year };
+
+type YearFilterProps = {
+  years: Year[];
+  setYear: Dispatch<SetStateAction<Year>>;
+  selectedOption: Year;
+};
+
+const YearFilter: FunctionComponent<YearFilterProps> = ({
+  years,
+  setYear,
+  selectedOption,
+}) => {
+  const options = years.map(year => ({ value: year, label: year }));
+  const value = { label: selectedOption, value: selectedOption };
+  return (
+    <Select
+      value={value}
+      onChange={(selectedYear: ValueType<OptionType>) =>
+        setYear((selectedYear as OptionType).value)
+      }
+      options={options}
+    />
+  );
+};
+
 export const LaunchList: FunctionComponent<LaunchListProps> = ({
   launches,
 }) => {
   const [sort, toggleSort] = useState<SortDirection>('asc');
   const [year, setYear] = useState<Year>(null);
+
+  const allYears = new Set<Year>();
+
+  launches.forEach(launch => {
+    allYears.add(launch.launch_year);
+  });
 
   const viewData = launches.filter(data =>
     year === null ? true : data.launch_year === year
@@ -24,16 +62,25 @@ export const LaunchList: FunctionComponent<LaunchListProps> = ({
   }
 
   return (
-    <ul>
-      {viewData.map((launch: SpaceXAPILaunch) => (
-        <LaunchItem
-          key={launch.mission_name}
-          missionName={launch.mission_name}
-          flightNumber={launch.flight_number}
-          rocketName={launch.rocket.rocket_name}
-          launchDateUTC={launch.launch_date_utc}
-        ></LaunchItem>
-      ))}
-    </ul>
+    <section>
+      <div>
+        <YearFilter
+          years={Array.from(allYears)}
+          setYear={setYear}
+          selectedOption={year}
+        />
+      </div>
+      <ul>
+        {viewData.map((launch: SpaceXAPILaunch) => (
+          <LaunchItem
+            key={launch.mission_name}
+            missionName={launch.mission_name}
+            flightNumber={launch.flight_number}
+            rocketName={launch.rocket.rocket_name}
+            launchDateUTC={launch.launch_date_utc}
+          ></LaunchItem>
+        ))}
+      </ul>
+    </section>
   );
 };
